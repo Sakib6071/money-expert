08c8afa68e6decc5e320bbd983e70c965f84188c
Your Name <you@example.com>
2022-02-18 20:00:48 +0600

    condition add for calculate button

--- js/script.js
+++ js/script.js
@@ -14,8 +14,19 @@ const rentCost=stringToFloat('rentCost');
 const clothesCost=stringToFloat('clothesCost');
 const totalCost=foodCost+rentCost+clothesCost;
 const balance=income-totalCost;
-const totalCostField = document.getElementById('totalExpensesValue');
-const balanceField = document.getElementById('currentBalanceValue');
-totalCostField.innerText=totalCost;
-balanceField.innerText=balance;
+if(income>=0 && foodCost>=0 && rentCost>=0 && clothesCost>=0){
+    if(totalCost<income){
+        const totalCostField = document.getElementById('totalExpensesValue');
+        const balanceField = document.getElementById('currentBalanceValue');
+        totalCostField.innerText=totalCost;
+        balanceField.innerText=balance;
+    }else{
+        alert('Sorry! your cost is higher than your income;')
+    }
+
+}
+else{
+    alert('Please Enter positive value. Income or Cost value is positive number')
+}
+
 })
