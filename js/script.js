4740b328c72719352772ce445f1a60a33a0f2906
Your Name <you@example.com>
2022-02-18 20:28:47 +0600

    Added Save button event and condition

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function stringToFloat(input){
     const inputField=document.getElementById(input);
     const inputFieldText=inputField.value;
     const inputValue = parseFloat(inputFieldText);
-    inputField.value='';
+    // inputField.value='';
     return inputValue;
 }
 
@@ -15,7 +15,7 @@ const clothesCost=stringToFloat('clothesCost');
 const totalCost=foodCost+rentCost+clothesCost;
 const balance=income-totalCost;
 if(income>=0 && foodCost>=0 && rentCost>=0 && clothesCost>=0){
-    if(totalCost<income){
+    if(totalCost<=income){
         const totalCostField = document.getElementById('totalExpensesValue');
         const balanceField = document.getElementById('currentBalanceValue');
         totalCostField.innerText=totalCost;
@@ -29,4 +29,30 @@ else{
     alert('Please Enter positive value. Income or Cost value is positive number')
 }
 
+})
+
+document.getElementById('save-btn').addEventListener('click',function(){
+    const income=stringToFloat('mainSalary');
+    const foodCost=stringToFloat('foodCost');
+    const rentCost=stringToFloat('rentCost');
+    const clothesCost=stringToFloat('clothesCost');
+    const totalCost=foodCost+rentCost+clothesCost;
+    const balance=income-totalCost;
+    const savePercent = stringToFloat('saveAmount');
+    if(savePercent>=0){
+        const saveAmount = income *(savePercent/100);
+        const remainig = balance-saveAmount;
+        if(remainig>=0){
+            const savingSet =document.getElementById('savingField');
+            const remainingSet = document.getElementById('remainingField');
+            savingSet.innerText=saveAmount;
+            remainingSet.innerText=remainig;
+        }else{
+            alert('Sorry!! Your have not sufficient balance for saving the ammount');
+        }
+    }
+    else{
+        alert("Your saving amount will be positive number");
+    }
+    
 })
